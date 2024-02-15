c7d332e8990eab518f1ab3b25f4819e239b40d73
Ireny Escobar <irenyescobar@outlook.com>
2024-02-15 15:58:21 -0400

    First steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/ireny/StartapTests.java
@@ -0,0 +1,13 @@
+package br.com.ireny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartapTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

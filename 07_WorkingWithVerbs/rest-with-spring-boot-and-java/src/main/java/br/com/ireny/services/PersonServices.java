0c62719cff3fb3422b7bd015a9aa9aa74b7535b2
Ireny Escobar <irenyescobar@outlook.com>
2024-02-15 20:20:59 -0400

    section 8

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java/src/main/java/br/com/ireny/services/PersonServices.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java/src/main/java/br/com/ireny/services/PersonServices.java
@@ -11,15 +11,15 @@ import br.com.ireny.model.Person;
 
 @Service
 public class PersonServices {
-	
+
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 	public List<Person> findAll() {
 
 		logger.info("Finding all people!");
-		
+
 		List<Person> persons = new ArrayList<>();
 		for (int i = 0; i < 8; i++) {
 			Person person = mockPerson(i);
@@ -29,9 +29,9 @@ public class PersonServices {
 	}
 
 	public Person findById(String id) {
-		
+
 		logger.info("Finding one person!");
-		
+
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Leandro");
@@ -40,28 +40,28 @@ public class PersonServices {
 		person.setGender("Male");
 		return person;
 	}
-	
+
 	public Person create(Person person) {
 
 		logger.info("Creating one person!");
-		
+
 		return person;
 	}
-	
+
 	public Person update(Person person) {
-		
+
 		logger.info("Updating one person!");
-		
+
 		return person;
 	}
-	
+
 	public void delete(String id) {
-		
+
 		logger.info("Deleting one person!");
 	}
-	
+
 	private Person mockPerson(int i) {
-		
+
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Person name " + i);
--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/ireny/model/Person.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java/src/main/java/br/com/ireny/model/Person.java
@@ -2,18 +2,38 @@ package br.com.ireny.model;
 
 import java.io.Serializable;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
-	private String lastName;
-	private String address;
-	private String gender;
 
-	public Person() {
-	}
+	@Column(name = "last_name", nullable = false, length = 80)
+	private String lastName;
+	
+	@Column(nullable = false, length = 100)
+	private String address;
+	
+	@Column(nullable = false, length = 6)
+	private String gender;
+	
+	public Person() {}
 
 	public Long getId() {
 		return id;
